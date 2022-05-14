2310260fc3d5647163508a5df995dac6b533e5b5
qing507543 <qingc@usc.edu>
2022-05-14 14:18:06 -0700

    new

--- Wk 3/script.js
+++ Wk 3/script.js
@@ -1,13 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = "";
+var allchars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 12;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * allchars.length);
+    password += allchars.substring(randomNumber, randomNumber +1);
+   }
 }
 
 // Add event listener to generate button
